e2f5875dc34617a2ab6104462a05404f4f857f69
Bhakti <sharmabhakti2611@gmail.com>
2023-12-03 22:55:49 +0530

    added prjects and about page

---
+++ portfolio/src/data.js
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: "Weather App",
+    subtitle: "HTML, CSS & JavaScript",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "../images/weatherApp.png",
+    link: "https://bhaktisharma26.github.io/weatherApp/",
+  },
+  {
+    title: "ToDo List",
+    subtitle: "HTML, CSS & JavaScript",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "../images/todo.png",
+    link: "https://bhaktisharma26.github.io/todo_list/",
+  },
+  {
+    title: "Color Generator",
+    subtitle: "HTML, CSS & JavaScript",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "../images/colorGenerator.png",
+    link: "https://bhaktisharma26.github.io/Random-color-generator/",
+  },
+];
