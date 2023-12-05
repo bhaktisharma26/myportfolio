525bd4bcce101a601d8bf7ffb71aae4bf732a785
Bhakti <sharmabhakti2611@gmail.com>
2023-12-05 08:55:18 +0530

    changes in About and Contact pages

--- portfolio/src/components/About.jsx
+++ portfolio/src/components/About.jsx
@@ -7,8 +7,8 @@ function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Hi, I'm Bhakti.
-            {/* <br className="hidden lg:inline-block" />I love to build amazing
-            apps. */}
+            <br className="hidden lg:inline-block" />I love to build amazing
+            apps.
           </h1>
           <p className="mb-8 leading-relaxed">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
@@ -30,7 +30,7 @@ function About() {
             </a>
           </div>
         </div>
-        <div className="lg:max-w-xl lg:w-full md:w-1/2 w-5/6">
+        <div className="lg:max-w-xl lg:w-full md:w-1/2 w-4/5">
           <img
             className="object-cover object-center rounded"
             alt="hero"
