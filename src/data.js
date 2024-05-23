4bdacc859dc92d895d8d4bcbc07aae443ab343d7
Bhakti <sharmabhakti2611@gmail.com>
2024-05-23 16:59:30 +0530

    changes in projects

--- src/data.js
+++ src/data.js
@@ -1,20 +1,17 @@
-
 export const projects = [
   {
-    title: "Resume Builder",
+    title: "Quill",
     subtitle: "ReactJs, BootStrap and ExpressJs",
-    description:
-      "Resume Builder where you can fill data and also download your resume",
-    image: "../images/weatherApp.png",
-    link: "https://bhaktisharma26.github.io/weatherApp/",
+    description: "A microblogging platform where you can share your thoughts",
+    image: "../images/quill.png",
+    link: "https://quill-dgkt.onrender.com/",
   },
   {
-    title: "Todo List",
+    title: "Talkify",
     subtitle: "Implements Redux Toolkit",
-    description:
-      "A simple todo list made to learn Redux ToolKit implementation in React",
-    image: "../images/todoRedux.png",
-    link: "https://github.com/bhaktisharma26/todoReduxToolkit.git",
+    description: "A chat app that lets you chat with people in realtime",
+    image: "../images/talkative.png",
+    link: "https://github.com/bhaktisharma26/chat_app2",
   },
   {
     title: "Google-Docs-Clone",
@@ -25,13 +22,21 @@ export const projects = [
     link: "https://github.com/bhaktisharma26/google-docs-clone.git",
   },
   {
-    title: "Todo List",
+    title: "Resume builder",
     subtitle: "Implements Redux Toolkit",
-    description:
-      "A simple todo list made to learn Redux ToolKit implementation in React",
-    image: "../images/todoRedux.png",
-    link: "https://github.com/bhaktisharma26/todoReduxToolkit.git",
+    description: "Make your resume by just putting in your skills",
+    image: "../images/resume_builder.png",
+    link: "https://github.com/bhaktisharma26/resume_builder.git",
   },
 ];
 
-export const skills = ["ReactJs", "JavaScript", "HTML", "CSS", "Node.Js", "Express.Js"];
+export const skills = [
+  "ReactJs",
+  "JavaScript",
+  "Node.Js",
+  "Express.Js",
+  "ChakraUi",
+  "DaisyUi",
+  "MongoDB",
+  "MySql",
+];
